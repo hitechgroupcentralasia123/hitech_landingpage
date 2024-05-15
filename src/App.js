980dda51185abb8c4cf0036b6989a7b1617c89f0
MarkennBagdat <bagdatmarkenn@gmail.com>
2024-05-15 23:55:03 +0500

    change-parners

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import MainPage from "./Pages/MainPage/index";
+import NewsPage from "./Pages/NewsPage/index";
+import "./styles/style.css";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Layout>
+                            <MainPage />
+                        </Layout>
+                    }
+                />
+                <Route
+                    path="/news"
+                    element={
+                        <Layout>
+                            <NewsPage />
+                        </Layout>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
