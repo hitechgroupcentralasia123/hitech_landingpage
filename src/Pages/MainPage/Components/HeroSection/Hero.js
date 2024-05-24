1c76349e6f20a7660f3e2f29e5b91470006474ab
MarkennBagdat <bagdatmarkenn@gmail.com>
2024-05-24 17:50:37 +0500

    change-parners

--- src/Pages/MainPage/Components/HeroSection/Hero.js
+++ src/Pages/MainPage/Components/HeroSection/Hero.js
@@ -14,6 +14,7 @@ function Hero() {
       <div className="hero_inner">
         <div className="hero__content">
           <div className="hero_main_text">
+            <div>
             <p className="hero_text_p">For a better world</p>
             <h1 className="hero_text_h1">To create green solutions</h1>
             <p className="hero_textfull_p">
@@ -25,17 +26,19 @@ function Hero() {
             <button className="hero__button_mobile">
             Contact us
           </button>
+          </div>
           <div className="hero_counter">
             <p>Our experience</p>
             <ExperienceCircles />
             </div>
+          </div>
           </div>
           <div className="hero__experience">
             <div className="hero_buttonpart">
               <button className="hero__button">
                 Find out more <img src={ArrowIcon} />
               </button>
-            </div>
+            
           </div>
         </div>
         </div>
