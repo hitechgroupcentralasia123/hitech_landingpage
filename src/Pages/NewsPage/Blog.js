a245d8993f4687728df3f3f2c6623cadad20b2d1
hitechgroupcentralasia123 <hitechgroupcentralasia123@gmail.com>
2024-07-13 11:29:10 +0500

    Добавлена папка .vercel

--- src/Pages/NewsPage/Blog.js
+++ src/Pages/NewsPage/Blog.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import client from "../../client";
 import "../../Assets/Styles/style.css";
 import LogoFull from "../../Assets/Images/NewsList/Frame.svg";
