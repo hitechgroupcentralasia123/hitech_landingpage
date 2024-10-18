a5f812b8d6364f83e0521849e58f90febbbeda46
hitechgroupcentralasia123 <hitechgroupcentralasia123@gmail.com>
2024-10-18 11:45:30 +0500

    gogogo

--- src/client.js
+++ src/client.js
@@ -1,8 +1,10 @@
-import client from '@sanity/client';
+import sanityClient from '@sanity/client';
 
-export default client({
+const client = sanityClient({
     projectId: 'ctrmv7z8', // Ваш project ID
     dataset: 'production', // Ваш dataset
-    useCdn: true,
+    useCdn: false, // false, чтобы получать последние данные
+    token: 'skve69rpuuu5QRaXj34ore1RPLH8cclx6pTbRT2uFnT8lLjFEymyCWLAyHJEFK8xkUkER76s7px6E2IjUflWOTybMAGBwcNpS2bjp8oirH7Kxs0NI7QfkAym5Xd6nSeJhNWFMxaTBndhujlomiZ30th0Dq7vtpu2MsUiagGhQpqShaEtR5DY' // Ваш API токен
 });
 
+export default client;
